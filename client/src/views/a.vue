75fee035ab0bc4f63fb927d8326c36f45eb5c45e
lisi <348070511@qq.com>
2021-01-18 17:41:23 +0800

    client

---
+++ client/src/views/a.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="a">
+    <el-form :inline="true" :model="formInline" class="demo-form-inline">
+      <el-form-item>
+        <el-input v-model="formInline.user" placeholder="姓名"></el-input>
+      </el-form-item>
+      <el-form-item>
+        <el-select v-model="formInline.region" placeholder="部门">
+          <el-option label="客服中心" value="a"></el-option>
+        </el-select>
+      </el-form-item>
+      <el-form-item>
+        <el-button type="primary" @click="onSubmit">录入</el-button>
+      </el-form-item>
+    </el-form>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'a',
+  data() {
+    return {
+      formInline: {
+        user: '',
+        region: ''
+      }
+    }
+  },
+  mounted() {
+
+  },
+  methods: {
+    onSubmit() {
+      console.log(this.formInline);
+    }
+  }
+}
+</script>
---
+++ client/src/views/home.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="home">
+    <el-container>
+      <el-header>
+        <el-menu :default-active="$route.path" mode="horizontal" :router='true'>
+          <el-menu-item index="/">考生录入</el-menu-item>
+          <el-menu-item index="/b">试卷配置</el-menu-item>
+        </el-menu>
+      </el-header>
+      <el-main>
+        <router-view></router-view>
+      </el-main>
+    </el-container>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'home',
+  data() {
+    return {
+      activeIndex: '/'
+    };
+  },
+  mounted() {
+
+  },
+  methods: {
+
+  }
+}
+</script>
