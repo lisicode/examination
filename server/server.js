8d8dc4e3bdd0efceb8a807f18905c3c6434c6e37
lisi <348070511@qq.com>
2021-01-22 00:10:20 +0800

    server

--- server/server.js
+++ server/server.js
@@ -1,45 +1,58 @@
 const http = require('http');
 const mysql = require('mysql');
 
-const connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'admin',
-    password: '123456',
-    database: 'examination'
-});
-connection.connect();
+const connection = () => {
+    let config = mysql.createConnection({
+        host: 'localhost',
+        user: 'admin',
+        password: '123456',
+        database: 'examination'
+    });
+    config.connect((err) => {
+        if (err) {
+            setTimeout('handleConnection()', 2000);
+        }
+    });
+    config.on('error', (err) => {
+        // console.log('db error', err);
+        if(err.code === 'PROTOCOL_CONNECTION_LOST') {
+            console.log('db error执行重连:'+err.message);
+            handleConnection();
+        } else {
+            throw err;
+        }
+    });
+    return config
+};
 
-let userGetSql = 'SELECT * FROM user WHERE account LIKE "13613787118"';
 
-connection.query(userGetSql,function (err, result) {
-    if(err){
-        console.log('[SELECT ERROR] - ',err.message);
-        return;
-    }
 
-    console.log('---------------SELECT----------------');
-    console.log(result);
-    console.log('$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$');
 
-});
-connection.end();
-
-http.createServer(function (req, res) {
-    req.on('data', function (e) {
-        let data = JSON.parse(e.toString())
+http.createServer((req, res) => {
+    req.on('data', (e) => {
+        let data = JSON.parse(e.toString());
         switch (data.api) {
             case 'S001':
-                console.log(data.signUpData)
+                console.log(data.signUpData.account);
 
+                let userGetSql = `SELECT * FROM user WHERE account LIKE ${data.signUpData.account}`;
+                connection().query(userGetSql, (err, result) => {
+                    if (err) {
+                        console.log('[SELECT ERROR] - ',err.message);
+                        return false;
+                    } else {
+                        if (result.length) {
+                            let sendData = {
+                                name: 'lisi'
+                            };
+                            res.end(JSON.stringify(sendData));
+                        } else {
+                            console.log(2)
+                        }
+                    }
+                });
                 break;
+
         }
-
-        req.on('end', function () {
-            let sendData = {
-                name: 'lisi'
-            }
-            res.end(JSON.stringify(sendData));
-        });
     });
-
 }).listen(8080);
