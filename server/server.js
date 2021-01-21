160e4d6ad2dc2f34dfb940f6775899c43ae781c1
lisi <348070511@qq.com>
2021-01-21 17:27:30 +0800

    server

--- server/server.js
+++ server/server.js
@@ -1,4 +1,28 @@
 const http = require('http');
+const mysql = require('mysql');
+
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'admin',
+    password: '123456',
+    database: 'examination'
+});
+connection.connect();
+
+let userGetSql = 'SELECT * FROM user WHERE account LIKE "13613787118"';
+
+connection.query(userGetSql,function (err, result) {
+    if(err){
+        console.log('[SELECT ERROR] - ',err.message);
+        return;
+    }
+
+    console.log('---------------SELECT----------------');
+    console.log(result);
+    console.log('$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$');
+
+});
+connection.end();
 
 http.createServer(function (req, res) {
     req.on('data', function (e) {
@@ -6,6 +30,7 @@ http.createServer(function (req, res) {
         switch (data.api) {
             case 'S001':
                 console.log(data.signUpData)
+
                 break;
         }
 
