ba1fa0b1fb9aefaab2bb9e88fb945794ac4db2d3
lisi <348070511@qq.com>
2021-01-22 12:48:31 +0800

    server

--- server/server.js
+++ server/server.js
@@ -10,14 +10,12 @@ const connection = () => {
     });
     config.connect((err) => {
         if (err) {
-            setTimeout('handleConnection()', 2000);
+            setTimeout('connection()', 2000);
         }
     });
     config.on('error', (err) => {
-        // console.log('db error', err);
         if(err.code === 'PROTOCOL_CONNECTION_LOST') {
-            console.log('db error执行重连:'+err.message);
-            handleConnection();
+            connection();
         } else {
             throw err;
         }
@@ -25,29 +23,36 @@ const connection = () => {
     return config
 };
 
-
-
-
 http.createServer((req, res) => {
     req.on('data', (e) => {
         let data = JSON.parse(e.toString());
         switch (data.api) {
             case 'S001':
-                console.log(data.signUpData.account);
-
-                let userGetSql = `SELECT * FROM user WHERE account LIKE ${data.signUpData.account}`;
-                connection().query(userGetSql, (err, result) => {
+                let queryUser = `SELECT * FROM user WHERE account LIKE ${data.signUpData.account}`;
+                let registeredUser = `INSERT INTO user(account, password, department, role) VALUES ('${data.signUpData.account}', '${data.signUpData.password}', '${data.signUpData.department}', '${data.signUpData.role}')`;
+                connection().query(queryUser, (err, result) => {
                     if (err) {
                         console.log('[SELECT ERROR] - ',err.message);
                         return false;
                     } else {
                         if (result.length) {
                             let sendData = {
-                                name: 'lisi'
+                                msg: '此用户已注册，请直接登录'
                             };
                             res.end(JSON.stringify(sendData));
                         } else {
-                            console.log(2)
+                            connection().query(registeredUser, (err, result) => {
+                                if (err) {
+                                    console.log('[SELECT ERROR] - ',err.message);
+                                    return false;
+                                } else {
+                                    let sendData = {
+                                        msg: '注册成功'
+                                    };
+                                    res.end(JSON.stringify(sendData));
+                                }
+                            });
+
                         }
                     }
                 });
